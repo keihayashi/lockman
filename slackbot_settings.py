a92f35c732fa59e2bd96bd50f8b377d4285b7e7f
keihayashi <keilee210@gmail.com>
2017-03-18 19:40:23 -0700

    feat: Made a simple slackbot
    Initial commit to check communication acknowledgement. Used environment variable SLACKBOT_API_TOKEN.
    reference: https://github.com/lins05/slackbot

---
+++ slackbot_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#import sys
+#
+#argvs = sys.argv
+#argc = len(argvs)
+#
+#if argc != 2:
+#	print 'Usage: python filename APIkey'
+#	quit()
+
+#API_TOKEN = SLACK_API_TOKEN
+default_reply = "I am sorry. I can not understand :("
